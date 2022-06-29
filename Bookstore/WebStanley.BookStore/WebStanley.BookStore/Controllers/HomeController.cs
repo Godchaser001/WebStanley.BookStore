9ca8d74eac30fa2a7ee4cf6a503eb8668022fa88
GodChaser <107036518+Godchaser001@users.noreply.github.com>
2022-06-29 18:10:33 +0100

    Added MVC to application

---
+++ Bookstore/WebStanley.BookStore/WebStanley.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStanley.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "hello stanley from webstanley";
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
--- Bookstore/WebStanley.BookStore/WebStanley.BookStore/Startup.cs
+++ Bookstore/WebStanley.BookStore/WebStanley.BookStore/Startup.cs
@@ -21,7 +21,9 @@ namespace WebStanley.BookStore
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {         
+
+            services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
@@ -43,9 +45,10 @@ namespace WebStanley.BookStore
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
+            app.UseEndpoints(endpoints =>  
+            {     
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapRazorPages();
             });
         }
     }
